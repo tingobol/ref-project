e94303ae75e70b50cb983984514f359b477faffd
kryptonite84sv@gmail.com <kryptonite84sv@gmail.com@b3be069d-38d6-5bde-4d73-1a18b43aa67a>
2012-10-09 04:39:41 +0000

    Se quita id auto incrementable a tabla articulo

---
+++ trunk/ejb-ref/ejbModule/com/dahg/project/ref/controller/services/local/ArticuloService.java
@@ -0,0 +1,11 @@
+package com.dahg.project.ref.controller.services.local;
+
+import javax.ejb.Local;
+
+import com.dahg.project.ref.controller.services.Service;
+import com.dahg.project.ref.model.Articulo;
+
+@Local
+public interface ArticuloService extends Service<Articulo>{
+
+}
