9f2dddbdb3768e3976a6c8ffb15d6f491342856e
kryptonite84sv <kryptonite84sv@gmail.com>
2012-10-15 03:31:08 +0000

    Desarrollo de mantenimiento de parametros varios

--- ref/src-test/com/dahg/project/ref/view/test/TestBean.java
+++ ref/src-test/com/dahg/project/ref/view/test/TestBean.java
@@ -1,18 +1,31 @@
 package com.dahg.project.ref.view.test;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
 
 import org.primefaces.context.RequestContext;
 
+import com.dahg.project.ref.controller.services.local.TextoService;
+import com.dahg.project.ref.model.parameters.Texto;
 import com.dahg.project.ref.view.AbstractManagedBean;
 
 @ManagedBean
 @ViewScoped
 public class TestBean extends AbstractManagedBean {
 	
+	@EJB
+	private TextoService pService;
+	
 	private String texto;
+	private Texto parametro;
+	
+	@PostConstruct
+	public void init() {
+		parametro = pService.getParametro("TEST");
+	}
 	
 	public boolean test(String arg) {		
 		return !(arg.indexOf("ADMIN")>-1);
@@ -34,6 +47,14 @@ public class TestBean extends AbstractManagedBean {
 
 	public void setTexto(String texto) {
 		this.texto = texto;
+	}
+
+	public Texto getParametro() {
+		return parametro;
+	}
+
+	public void setParametro(Texto parametro) {
+		this.parametro = parametro;
 	}
 	
 	
