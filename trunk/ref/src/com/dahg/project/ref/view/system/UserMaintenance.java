b6635baff55a10a77fa3d051c6ee5164776e1f83
kryptonite84sv@gmail.com <kryptonite84sv@gmail.com@b3be069d-38d6-5bde-4d73-1a18b43aa67a>
2012-12-05 22:35:03 +0000

    Funcionalidad de agregar nuevo usuario

--- trunk/ref/src/com/dahg/project/ref/view/system/UserMaintenance.java
+++ trunk/ref/src/com/dahg/project/ref/view/system/UserMaintenance.java
@@ -7,6 +7,10 @@ import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.RowEditEvent;
+
+import com.dahg.project.ref.controller.exception.ControllerException;
 import com.dahg.project.ref.controller.services.local.UsuarioService;
 import com.dahg.project.ref.model.impl.Usuario;
 import com.dahg.project.ref.view.AbstractManagedBean;
@@ -18,10 +22,35 @@ public class UserMaintenance extends AbstractManagedBean {
 	@EJB
 	private UsuarioService service;
 	private List<Usuario> all;
+	private String id;
 	
 	@PostConstruct
 	public void init() {
 		all = service.getAll();
+	}
+	
+	public String add() {
+		if(id==null || id.isEmpty()) addError("Debe ingresar un ID");
+		else {
+			Usuario usuario = new Usuario();
+			usuario.setUsername(getId());
+			try {
+				service.persist(usuario);
+				all.clear();
+				all.addAll(service.getAll());
+				RequestContext rc = getRequestContext();
+				rc.execute("edit.hide()");
+			} catch (ControllerException e) {
+				addError(e);
+			}
+		}
+		return null;
+	}
+	
+	public void editUser(RowEditEvent evt) {
+		Usuario u = (Usuario) evt.getObject();
+		service.merge(u);
+		addInfo(String.format("%s actualizado", u.getUsername()));
 	}
 	
 	public List<Usuario> getAll() {
@@ -29,5 +58,13 @@ public class UserMaintenance extends AbstractManagedBean {
 	}
 	public void setAll(List<Usuario> all) {
 		this.all = all;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 }
