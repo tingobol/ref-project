59e56e03b28044d4ce414dd927170931e1995936
kryptonite84sv@gmail.com <kryptonite84sv@gmail.com@b3be069d-38d6-5bde-4d73-1a18b43aa67a>
2013-01-21 22:16:40 +0000

    Nueva implementación para la autorizacion de paginas

--- trunk/ejb-ref/ejbModule/com/dahg/project/ref/controller/services/local/UsuarioService.java
+++ trunk/ejb-ref/ejbModule/com/dahg/project/ref/controller/services/local/UsuarioService.java
@@ -16,4 +16,5 @@ public interface UsuarioService extends Service<Usuario>{
 	Usuario validate(String user, String pass) throws ValidationException,ControllerException;
 	void changePassword(Usuario usuario, String oldPassword, String newPassword) throws ValidationException;
 	void changePassword(Usuario usuario, String newPassword) throws ValidationException;
+	void isAuthorized(String section,Usuario usuario) throws ValidationException;
 }
---
+++ trunk/ejb-ref/ejbModule/com/dahg/project/ref/model/impl/AutorizacionVista.java
@@ -0,0 +1,53 @@
+package com.dahg.project.ref.model.impl;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="autorizacion_vista")
+public class AutorizacionVista implements Serializable {
+
+	@Id
+	private String seccion;
+	private String roles;
+	public String getSeccion() {
+		return seccion;
+	}
+	public void setSeccion(String seccion) {
+		this.seccion = seccion;
+	}
+	public String getRoles() {
+		return roles;
+	}
+	public void setRoles(String roles) {
+		this.roles = roles;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((seccion == null) ? 0 : seccion.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AutorizacionVista other = (AutorizacionVista) obj;
+		if (seccion == null) {
+			if (other.seccion != null)
+				return false;
+		} else if (!seccion.equals(other.seccion))
+			return false;
+		return true;
+	}
+	
+	
+}
